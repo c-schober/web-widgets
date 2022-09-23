80fea750addb8681ab8b9337f91ff7bea32ad5ce
Christoph Schober <info@christoph-schober.com>
2022-09-23 15:37:47 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+const roots = []
+const rootNodes = document.querySelectorAll('.ed-react-widget')
+rootNodes.forEach((node) => roots.push(ReactDOM.createRoot(node)))
+roots.forEach((root) =>
+  root.render(
+    <React.StrictMode>
+      <App domElement={rootNodes[0]} />
+    </React.StrictMode>
+  )
+)
